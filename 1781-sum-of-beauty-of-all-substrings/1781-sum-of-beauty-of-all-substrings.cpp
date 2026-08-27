3045dae062a2fa8dcb1b66abbfb7ba01e91eb3f3
Lakshay <161314423+lakshayxdev@users.noreply.github.com>
2026-08-27 23:21:49 +0530

    Time: 125 ms (25.54%), Space: 26.1 MB (30%) - LeetHub

---
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int beautySum(string s) {
+        int sum=0;
+        for(int i=0; i<s.size(); i++) {
+            unordered_map<char,int> mp;
+            for(int j=i; j<s.size(); j++) {
+               mp[s[j]]++;
+                int maxi=INT_MIN;
+                int mini=INT_MAX;
+                for(auto it:mp) {
+                maxi=max(maxi,it.second);
+                mini=min(mini,it.second);
+                }
+                sum+=maxi-mini;
+            }
+        }
+        return sum;
+    }
+};
