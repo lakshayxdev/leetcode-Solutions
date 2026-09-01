be6e681be774e16ddbd68a28bdd17d989c161693
Lakshay <161314423+lakshayxdev@users.noreply.github.com>
2026-09-02 00:51:14 +0530

    Time: 166 ms (29.44%), Space: 129.7 MB (28.17%) - LeetHub

---
+++ 2762-continuous-subarrays/2762-continuous-subarrays.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        long long count=0;
+        priority_queue<pair<int,int>> pq1;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq2;
+         int l = 0;
+
+        for(int r = 0; r < nums.size(); r++) {
+
+            pq1.push({nums[r], r});
+            pq2.push({nums[r], r});
+            while(pq1.top().first - pq2.top().first > 2) {
+
+    l++;
+
+    while(!pq1.empty() && pq1.top().second < l)
+        pq1.pop();
+
+    while(!pq2.empty() && pq2.top().second < l)
+        pq2.pop();
+}
+count+=r-l+1;
+        }
+        return count;
+    }
+};
