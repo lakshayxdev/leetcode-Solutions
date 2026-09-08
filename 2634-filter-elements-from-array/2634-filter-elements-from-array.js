54f6e07be4991de49877f88061861bbea0186582
Lakshay <161314423+lakshayxdev@users.noreply.github.com>
2026-09-08 20:16:30 +0530

    Time: 44 ms (52.04%), Space: 53.3 MB (67.28%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    let nums=[];
+    for(let i=0; i<arr.length; i++) {
+        let result=fn(arr[i],i);
+        if(result) {
+            nums.push(arr[i]);
+        }
+    }
+    return nums;
+
+};
